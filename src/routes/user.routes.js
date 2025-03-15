454cb0531726e5a8b721d67da17200e53acb0b46
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2025-03-16 02:06:13 +0500

    add authentication middleware and update user routes for login and logout functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
+import { verifyJwt } from "../middleware/auth.middleware.js";
 const router = Router();
 console.log("router", router);
 router.route("/register").post(
@@ -11,4 +17,10 @@ router.route("/register").post(
   registerUser,
 );
 
+router.route("/login").post(loginUser);
+
+//secure route
+router.route("/logout").post(verifyJwt, logoutUser);
+
+router.route("refresh-token").post(refreshAccessToken);
 export default router;
